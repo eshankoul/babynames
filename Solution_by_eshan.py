4449f55141cfca50eb3db5a5c7ed0e010071d4f7
eshankoul <eshan.koul@gmail.com>
2016-07-19 22:21:37 -0700

    final version, idea taken from google solution

---
+++ Solution_by_eshan.py
@@ -0,0 +1,24 @@
+import re
+#filename = input("Enter the file name\n")
+f = open('baby1990.html','r')
+names = f.read()
+#print(names)
+year=0
+year = re.findall(r'(\d\d\d\d)</h3>',names)
+extract=[]
+extract.append(year[0])
+#print(extract)
+
+tuples= re.findall(r'<td>([\w]+)</td>+<td>([\w]+)</td>+<td>([\w]+)</td>',names)
+dict={}
+for tuple in tuples:
+    dict[tuple[1]]=tuple[0]
+    dict[tuple[2]]=tuple[0]
+
+    # print(tuple[1]+' '+str(tuple[0]))
+    # extract.append(tuple[1]+' '+str(tuple[0]))
+
+dict_final=sorted(dict.keys())
+for name in dict_final:
+    extract.append(name+' '+dict[name])
+print(extract)
